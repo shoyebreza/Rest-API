9d25834dad6c8b2f925e9f02a112bae4c54325d0
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-10-03 23:29:14 +0600

    first commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAuthorRequest;
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $authors  = Author::paginate(10);
+        return response()->json([
+            'authors' =>$authors,
+            'message' =>'author fached success'
+        ],200);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAuthorRequest $request)
+    {
+        $author = Author::create($request->validated());
+
+        /* $author = Author::create([
+            'name'=>$request->name,
+            'bio'=>$request->bio,
+            'nationality'=>$request->nationality
+        ]); */
+
+        return response()->json([
+            'author' =>$author,
+            'status' =>'Author created successfully'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Author $author)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Author $author)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Author $author)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Author $author)
+    {
+        //
+    }
+}
